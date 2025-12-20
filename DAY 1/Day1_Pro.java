ba37a56b4bb627393f50fed12e5bfba047d86e46
782-lab <mx08264@gmail.com>
2025-12-20 19:48:07 +0530

    Day1_Program2_PersonalDetails.java

---
+++ DAY 1/Day1_Pro.java
@@ -0,0 +1,7 @@
+class Day1_Program2_PersonalDetails {
+    public static void main(String[] args) {
+        System.out.println("Name  : Md Ashab Akhtar");
+        System.out.println("Course: B.Tech CSE");
+        System.out.println("Year  : 3rd Year");
+    }
+}
