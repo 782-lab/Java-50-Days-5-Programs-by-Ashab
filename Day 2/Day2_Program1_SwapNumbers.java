73c7b1e7a0f4336dffd780866cf508162e4de46b
782-lab <mx08264@gmail.com>
2025-12-21 12:56:47 +0530

    Day2_Program1_SwapNumbers.java

---
+++ Day 2/Day2_Program1_SwapNumbers.java
@@ -0,0 +1,16 @@
+class Day2_Program1_SwapNumbers {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int temp;
+
+        temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println("After Swapping:");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+}
+
