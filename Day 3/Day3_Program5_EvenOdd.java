e789cb5c38dc167256a1c328b0806edc67095ccd
782-lab <mx08264@gmail.com>
2025-12-22 16:53:34 +0530

    Day3_Program5_EvenOdd.java

---
+++ Day 3/Day3_Program5_EvenOdd.java
@@ -0,0 +1,11 @@
+class Day3_Program5_EvenOdd {
+    public static void main(String[] args) {
+        int number = 27;
+
+        if (number % 2 == 0) {
+            System.out.println(number + " is Even");
+        } else {
+            System.out.println(number + " is Odd");
+        }
+    }
+}
