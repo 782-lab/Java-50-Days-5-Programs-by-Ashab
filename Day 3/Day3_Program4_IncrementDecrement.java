c2c198af425a1aed3a68967f45091199db0ef311
782-lab <mx08264@gmail.com>
2025-12-22 16:52:27 +0530

    Day3_Program4_IncrementDecrement.java

---
+++ Day 3/Day3_Program4_IncrementDecrement.java
@@ -0,0 +1,17 @@
+class Day3_Program4_IncrementDecrement {
+    public static void main(String[] args) {
+        int a = 10;
+
+        System.out.println("Post Increment: " + (a++));
+        System.out.println("After Post Increment: " + a);
+
+        System.out.println("Pre Increment: " + (++a));
+        System.out.println("After Pre Increment: " + a);
+
+        System.out.println("Post Decrement: " + (a--));
+        System.out.println("After Post Decrement: " + a);
+
+        System.out.println("Pre Decrement: " + (--a));
+    }
+}
+
