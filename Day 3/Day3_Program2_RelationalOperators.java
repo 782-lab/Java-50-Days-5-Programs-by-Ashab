1691d336b52fab68ec8d7de1807d248fc930b3ef
782-lab <mx08264@gmail.com>
2025-12-22 16:49:39 +0530

    Day3_Program2_RelationalOperators.java

---
+++ Day 3/Day3_Program2_RelationalOperators.java
@@ -0,0 +1,14 @@
+class Day3_Program2_RelationalOperators {
+    public static void main(String[] args) {
+        int x = 15;
+        int y = 20;
+
+        System.out.println("x == y : " + (x == y));
+        System.out.println("x != y : " + (x != y));
+        System.out.println("x > y  : " + (x > y));
+        System.out.println("x < y  : " + (x < y));
+        System.out.println("x >= y : " + (x >= y));
+        System.out.println("x <= y : " + (x <= y));
+    }
+}
+
