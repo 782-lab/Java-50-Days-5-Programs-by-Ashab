b68253318935b4e70446b31e71c00cb2299e0f2e
782-lab <mx08264@gmail.com>
2025-12-24 19:15:09 +0530

    Day4_Program1_PositiveNegative.java

---
+++ Day 4/Day4_Program1_PositiveNegative.java
@@ -0,0 +1,14 @@
+class Day4_Program1_PositiveNegative {
+    public static void main(String[] args) {
+        int number = -12;
+
+        if (number > 0) {
+            System.out.println(number + " is Positive");
+        } else if (number < 0) {
+            System.out.println(number + " is Negative");
+        } else {
+            System.out.println("Number is Zero");
+        }
+    }
+}
+
