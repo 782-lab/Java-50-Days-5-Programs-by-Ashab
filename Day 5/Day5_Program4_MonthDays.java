3b0b85b039984fc7ab772fe7cc2592c179e74971
782-lab <mx08264@gmail.com>
2025-12-25 20:03:23 +0530

    Day5_Program4_GradeSystem.java

---
+++ Day 5/Day5_Program4_MonthDays.java
@@ -0,0 +1,26 @@
+class Day5_Program4_GradeSystem {
+    public static void main(String[] args) {
+
+        char grade = 'A';
+
+        switch (grade) {
+            case 'A':
+                System.out.println("Excellent");
+                break;
+            case 'B':
+                System.out.println("Very Good");
+                break;
+            case 'C':
+                System.out.println("Good");
+                break;
+            case 'D':
+                System.out.println("Average");
+                break;
+            case 'F':
+                System.out.println("Fail");
+                break;
+            default:
+                System.out.println("Invalid Grade");
+        }
+    }
+}
