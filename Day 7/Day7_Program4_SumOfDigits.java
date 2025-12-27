3b0db56515b66e4b5ac7551164daa8ca7e71377d
782-lab <mx08264@gmail.com>
2025-12-27 15:45:15 +0530

    Day7_Program4_SumOfDigits.java

---
+++ Day 7/Day7_Program4_SumOfDigits.java
@@ -0,0 +1,13 @@
+class Day7_Program4_SumOfDigits {
+    public static void main(String[] args) {
+        int num = 456;
+        int sum = 0;
+
+        while (num != 0) {
+            sum = sum + (num % 10);
+            num = num / 10;
+        }
+
+        System.out.println("Sum of Digits = " + sum);
+    }
+}
