a2514d5ca38daa1703a7aadb5a843836ed51c0a3
782-lab <mx08264@gmail.com>
2025-12-31 18:35:11 +0530

    Day11_Program3_MaxElement.java

---
+++ Day 11/Day11_Program3_MaxElement.java
@@ -0,0 +1,14 @@
+class Day11_Program3_MaxElement {
+    public static void main(String[] args) {
+        int[] arr = {25, 60, 15, 90, 45};
+        int max = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+
+        System.out.println("Maximum Element = " + max);
+    }
+}
