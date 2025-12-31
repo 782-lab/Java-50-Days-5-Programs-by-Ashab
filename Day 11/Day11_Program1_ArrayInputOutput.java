08a8159266e1f17835d4e678eed8e596b8018220
782-lab <mx08264@gmail.com>
2025-12-31 18:33:00 +0530

    Day11_Program1_ArrayInputOutput.java

---
+++ Day 11/Day11_Program1_ArrayInputOutput.java
@@ -0,0 +1,10 @@
+class Day11_Program1_ArrayInputOutput {
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 50};
+
+        System.out.println("Array Elements:");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
