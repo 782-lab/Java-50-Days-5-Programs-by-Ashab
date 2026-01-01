6a91e9f40372ea916bd0b1182c7e844200569826
782-lab <mx08264@gmail.com>
2026-01-01 18:18:17 +0530

    Day12_Program5_DuplicateElements.java

---
+++ Day 12/Day12_Program5_DuplicateElements.java
@@ -0,0 +1,15 @@
+class Day12_Program5_DuplicateElements {
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 20, 10};
+
+        System.out.println("Duplicate Elements:");
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] == arr[j]) {
+                    System.out.println(arr[i]);
+                    break;
+                }
+            }
+        }
+    }
+}
