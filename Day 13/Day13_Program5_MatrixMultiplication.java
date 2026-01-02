0277df83268eac0d3a808e0f17f9f2a8e9f8fd7b
782-lab <mx08264@gmail.com>
2026-01-02 18:07:05 +0530

    Day13_Program5_MatrixMultiplication.java

---
+++ Day 13/Day13_Program5_MatrixMultiplication.java
@@ -0,0 +1,32 @@
+class Day13_Program5_MatrixMultiplication {
+    public static void main(String[] args) {
+        int[][] a = {
+            {1, 2},
+            {3, 4}
+        };
+
+        int[][] b = {
+            {2, 0},
+            {1, 2}
+        };
+
+        int[][] result = new int[2][2];
+
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                for (int k = 0; k < 2; k++) {
+                    result[i][j] += a[i][k] * b[k][j];
+                }
+            }
+        }
+
+        System.out.println("Matrix Multiplication:");
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                System.out.print(result[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
