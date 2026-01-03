174489e4628a69acd7caa0eb967704ad21c9b69f
782-lab <mx08264@gmail.com>
2026-01-03 18:39:50 +0530

    Day14_Program1_StringInputOutput.java

---
+++ Day 14/Day14_Program1_StringInputOutput.java
@@ -0,0 +1,8 @@
+class Day14_Program1_StringInputOutput {
+    public static void main(String[] args) {
+        String str = "Hello Java";
+
+        System.out.println("String = " + str);
+    }
+}
+
