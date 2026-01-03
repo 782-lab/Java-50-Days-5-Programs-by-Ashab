93d429a50fc0c96f83ec3fa080ec1a62baf956d7
782-lab <mx08264@gmail.com>
2026-01-03 18:42:33 +0530

    Day14_Program3_ReverseString.java

---
+++ Day 14/Day14_Program3_ReverseString.java
@@ -0,0 +1,13 @@
+class Day14_Program3_ReverseString {
+    public static void main(String[] args) {
+        String str = "Java";
+        String reverse = "";
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reverse = reverse + str.charAt(i);
+        }
+
+        System.out.println("Reversed String = " + reverse);
+    }
+}
+
