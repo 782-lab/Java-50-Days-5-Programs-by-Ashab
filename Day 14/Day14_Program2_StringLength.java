d34b6da83d647ffc45ea0a168af54290e53e6358
782-lab <mx08264@gmail.com>
2026-01-03 18:41:26 +0530

    Day14_Program2_StringLength.java

---
+++ Day 14/Day14_Program2_StringLength.java
@@ -0,0 +1,8 @@
+
+class Day14_Program2_StringLength {
+    public static void main(String[] args) {
+        String str = "Programming";
+
+        System.out.println("Length of String = " + str.length());
+    }
+}
