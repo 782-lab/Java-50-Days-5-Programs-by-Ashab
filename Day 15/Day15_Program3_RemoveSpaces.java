35d15c575154e15ffebe02bb7c67cf73599c4b9b
782-lab <mx08264@gmail.com>
2026-01-04 13:49:36 +0530

    Day15_Program3_RemoveSpaces.java

---
+++ Day 15/Day15_Program3_RemoveSpaces.java
@@ -0,0 +1,9 @@
+class Day15_Program3_RemoveSpaces {
+    public static void main(String[] args) {
+        String str = "Java Programming Language";
+
+        String result = str.replace(" ", "");
+
+        System.out.println("String without spaces = " + result);
+    }
+}
