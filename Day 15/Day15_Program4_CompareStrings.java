a14c9771b71ac5fb65aa9f714e242a64d3e7045b
782-lab <mx08264@gmail.com>
2026-01-04 13:50:51 +0530

    Day15_Program4_CompareStrings.java

---
+++ Day 15/Day15_Program4_CompareStrings.java
@@ -0,0 +1,12 @@
+class Day15_Program4_CompareStrings {
+    public static void main(String[] args) {
+        String str1 = "Java";
+        String str2 = "Java";
+
+        if (str1.equals(str2)) {
+            System.out.println("Strings are Equal");
+        } else {
+            System.out.println("Strings are NOT Equal");
+        }
+    }
+}
