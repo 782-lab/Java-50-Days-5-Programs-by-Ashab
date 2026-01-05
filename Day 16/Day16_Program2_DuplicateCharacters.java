da5f0257d5cfeb3c7ad11e9a5c644519c511fb69
782-lab <mx08264@gmail.com>
2026-01-05 23:23:18 +0530

    Day16_Program2_DuplicateCharacters.java

---
+++ Day 16/Day16_Program2_DuplicateCharacters.java
@@ -0,0 +1,15 @@
+class Day16_Program2_DuplicateCharacters {
+    public static void main(String[] args) {
+        String str = "programming";
+
+        System.out.println("Duplicate Characters:");
+        for (int i = 0; i < str.length(); i++) {
+            for (int j = i + 1; j < str.length(); j++) {
+                if (str.charAt(i) == str.charAt(j)) {
+                    System.out.println(str.charAt(i));
+                    break;
+                }
+            }
+        }
+    }
+}
