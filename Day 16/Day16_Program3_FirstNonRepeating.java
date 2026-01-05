dc75d4992eeb668edc0d39ea977760f3b8e648c5
782-lab <mx08264@gmail.com>
2026-01-05 23:24:18 +0530

    Day16_Program3_FirstNonRepeating.java

---
+++ Day 16/Day16_Program3_FirstNonRepeating.java
@@ -0,0 +1,22 @@
+class Day16_Program3_FirstNonRepeating {
+    public static void main(String[] args) {
+        String str = "swiss";
+
+        for (int i = 0; i < str.length(); i++) {
+            boolean unique = true;
+
+            for (int j = 0; j < str.length(); j++) {
+                if (i != j && str.charAt(i) == str.charAt(j)) {
+                    unique = false;
+                    break;
+                }
+            }
+
+            if (unique) {
+                System.out.println("First Non-Repeating Character = " + str.charAt(i));
+                break;
+            }
+        }
+    }
+}
+
