fecab28481d1be6b5015f8e8f25b5167891f2a24
782-lab <mx08264@gmail.com>
2026-01-07 02:16:12 +0530

    Day18_Program1_SumCLA.java

---
+++ Day 18/Day18_Program1_SumCLA.java
@@ -0,0 +1,11 @@
+class Day18_Program1_SumCLA {
+    public static void main(String[] args) {
+
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+
+        int sum = a + b;
+
+        System.out.println("Sum = " + sum);
+    }
+}
