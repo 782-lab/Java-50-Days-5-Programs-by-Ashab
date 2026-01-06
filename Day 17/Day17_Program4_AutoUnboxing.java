b4e68e3de46b831e6b63f49aa8ab6a26454573f5
782-lab <mx08264@gmail.com>
2026-01-06 19:14:03 +0530

    Day17_Program4_AutoUnboxing.java

---
+++ Day 17/Day17_Program4_AutoUnboxing.java
@@ -0,0 +1,10 @@
+class Day17_Program4_AutoUnboxing {
+    public static void main(String[] args) {
+        Integer obj = 100;
+
+        int a = obj; // Auto-unboxing
+
+        System.out.println("Auto-unboxed int = " + a);
+    }
+}
+
