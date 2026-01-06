865e93f7be82da61bb3f8269506e7d3164aaa26a
782-lab <mx08264@gmail.com>
2026-01-06 19:12:34 +0530

    Day17_Program3_Autoboxing.java

---
+++ Day 17/Day17_Program3_Autoboxing.java
@@ -0,0 +1,9 @@
+class Day17_Program3_Autoboxing {
+    public static void main(String[] args) {
+        int a = 50;
+
+        Integer obj = a; // Autoboxing
+
+        System.out.println("Autoboxed Integer = " + obj);
+    }
+}
