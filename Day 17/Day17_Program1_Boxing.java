37aec2c8923912ce4b00d46b478f0238c35abef8
782-lab <mx08264@gmail.com>
2026-01-06 19:10:04 +0530

    Day17_Program1_Boxing.java

---
+++ Day 17/Day17_Program1_Boxing.java
@@ -0,0 +1,10 @@
+class Day17_Program1_Boxing {
+    public static void main(String[] args) {
+        int a = 10;
+
+        Integer obj = Integer.valueOf(a); // Boxing
+
+        System.out.println("Primitive int = " + a);
+        System.out.println("Wrapper Integer = " + obj);
+    }
+}
