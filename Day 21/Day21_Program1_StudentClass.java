16ee115797e93a2d0d5efa2f31ca944a7ec6c3ba
782-lab <mx08264@gmail.com>
2026-01-11 23:10:49 +0530

    Day21_Program1_StudentClass.java

---
+++ Day 21/Day21_Program1_StudentClass.java
@@ -0,0 +1,19 @@
+class Student {
+    String name;
+    int roll;
+
+    void display() {
+        System.out.println("Name: " + name);
+        System.out.println("Roll: " + roll);
+    }
+}
+
+class Day21_Program1_StudentClass {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.name = "Ashab";
+        s1.roll = 101;
+
+        s1.display();
+    }
+}
