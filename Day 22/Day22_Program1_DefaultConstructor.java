cb976380837874c513fe659d01df85f56ac36316
782-lab <mx08264@gmail.com>
2026-01-13 23:34:51 +0530

    Day22_Program1_DefaultConstructor.java

---
+++ Day 22/Day22_Program1_DefaultConstructor.java
@@ -0,0 +1,11 @@
+class Student {
+    Student() {
+        System.out.println("Default Constructor Called");
+    }
+}
+
+class Day22_Program1_DefaultConstructor {
+    public static void main(String[] args) {
+        Student s = new Student();
+    }
+}
