9b06feb3b64a3fa88869d0b942d74371ede85e85
782-lab <mx08264@gmail.com>
2026-01-15 22:33:14 +0530

    Day24_Program3_ShapeArea.java

---
+++ Day 24/Day24_Program3_ShapeArea.java
@@ -0,0 +1,32 @@
+class Shape {
+    double area() {
+        return 0;
+    }
+}
+
+class Rectangle extends Shape {
+    double length = 10, breadth = 5;
+
+    double area() {
+        return length * breadth;
+    }
+}
+
+class Circle extends Shape {
+    double radius = 7;
+
+    double area() {
+        return 3.14 * radius * radius;
+    }
+}
+
+class Day24_Program3_ShapeArea {
+    public static void main(String[] args) {
+        Shape s1 = new Rectangle();
+        Shape s2 = new Circle();
+
+        System.out.println("Rectangle Area = " + s1.area());
+        System.out.println("Circle Area = " + s2.area());
+    }
+}
+
