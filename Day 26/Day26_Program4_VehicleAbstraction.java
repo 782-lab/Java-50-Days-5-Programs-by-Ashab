dab1e3b4505795e33e0d8c1b120fe401e31d4aac
782-lab <mx08264@gmail.com>
2026-01-19 22:23:00 +0530

    Day26_Program4_VehicleAbstraction.java

---
+++ Day 26/Day26_Program4_VehicleAbstraction.java
@@ -0,0 +1,25 @@
+abstract class Vehicle {
+    abstract void start();
+}
+
+class Car extends Vehicle {
+    void start() {
+        System.out.println("Car starts with key");
+    }
+}
+
+class Bike extends Vehicle {
+    void start() {
+        System.out.println("Bike starts with kick");
+    }
+}
+
+class Day26_Program4_VehicleAbstraction {
+    public static void main(String[] args) {
+        Vehicle v1 = new Car();
+        Vehicle v2 = new Bike();
+
+        v1.start();
+        v2.start();
+    }
+}
