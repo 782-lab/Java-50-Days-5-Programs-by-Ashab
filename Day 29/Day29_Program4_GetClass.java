b047a176efe20ce3e1ee1c7f50075a3fa95cc8a1
782-lab <mx08264@gmail.com>
2026-01-23 23:28:17 +0530

    Day29_Program4_GetClass.java

---
+++ Day 29/Day29_Program4_GetClass.java
@@ -0,0 +1,9 @@
+class Demo {
+}
+
+class Day29_Program4_GetClass {
+    public static void main(String[] args) {
+        Demo d = new Demo();
+        System.out.println("Class Name = " + d.getClass().getName());
+    }
+}
