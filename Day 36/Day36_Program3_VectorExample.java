d9b5b4756b8335468795b1290d836a45f47a0164
782-lab <mx08264@gmail.com>
2026-02-05 23:51:00 +0530

    Day36_Program3_VectorExample.java

---
+++ Day 36/Day36_Program3_VectorExample.java
@@ -0,0 +1,14 @@
+import java.util.Vector;
+
+class Day36_Program3_VectorExample {
+    public static void main(String[] args) {
+        Vector<String> vector = new Vector<>();
+
+        vector.add("Red");
+        vector.add("Green");
+        vector.add("Blue");
+
+        System.out.println("Vector Elements:");
+        System.out.println(vector);
+    }
+}
