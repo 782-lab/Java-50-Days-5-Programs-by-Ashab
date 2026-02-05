78bb03e545dc3bca5daeacc3e4515775bf0d73d9
782-lab <mx08264@gmail.com>
2026-02-05 23:49:37 +0530

    Day36_Program2_LinkedListExample.java

---
+++ Day 36/Day36_Program2_LinkedListExample.java
@@ -0,0 +1,14 @@
+import java.util.LinkedList;
+
+class Day36_Program2_LinkedListExample {
+    public static void main(String[] args) {
+        LinkedList<Integer> list = new LinkedList<>();
+
+        list.add(10);
+        list.add(20);
+        list.add(30);
+
+        System.out.println("LinkedList Elements:");
+        System.out.println(list);
+    }
+}
