cfb350f5d5154a528bcac94b47d95745028bc7a9
782-lab <mx08264@gmail.com>
2026-02-13 23:22:46 +0530

    Day43_Program2_LocalTime.java

---
+++ Day 43/Day43_Program2_LocalTime.java
@@ -0,0 +1,11 @@
+import java.time.LocalTime;
+
+class Day43_Program2_LocalTime {
+    public static void main(String[] args) {
+
+        LocalTime time = LocalTime.now();
+
+        System.out.println("Current Time: " + time);
+    }
+}
+
