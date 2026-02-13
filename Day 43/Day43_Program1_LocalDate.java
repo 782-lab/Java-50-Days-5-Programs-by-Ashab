e1b76715de11a56489b135506c95f802c23bbf7c
782-lab <mx08264@gmail.com>
2026-02-13 23:21:54 +0530

    Day43_Program1_LocalDate.java

---
+++ Day 43/Day43_Program1_LocalDate.java
@@ -0,0 +1,11 @@
+import java.time.LocalDate;
+
+class Day43_Program1_LocalDate {
+    public static void main(String[] args) {
+
+        LocalDate today = LocalDate.now();
+
+        System.out.println("Today's Date: " + today);
+    }
+}
+
