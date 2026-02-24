e3cc92a661167b2d0415e6b1edfc9596351b3247
Md Ashab Akhtar <153938074+782-lab@users.noreply.github.com>
2026-02-24 23:30:13 +0530

    Update Day12_Program2_SortArray.java

--- Day 12/Day12_Program2_SortArray.java
+++ Day 12/Day12_Program2_SortArray.java
@@ -1,5 +1,6 @@
 class Day12_Program2_SortArray {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         int[] arr = {40, 10, 50, 20, 30};
 
         for (int i = 0; i < arr.length - 1; i++) {
